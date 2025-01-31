bfa72cc34f57556fe4e920b825f39924e9bc121c
yuri.delima <yuri.delima@enghouse.com>
2025-01-30 23:56:28 -0300

    Skeleton first commit.

---
+++ src/main/java/br/com/inno/establishment_registration_service/EstablishmentRegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.inno.establishment_registration_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstablishmentRegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstablishmentRegistrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/inno/establishment_registration_service/EstablishmentRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.inno.establishment_registration_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstablishmentRegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
